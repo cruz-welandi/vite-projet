b944fffc05893317b92cc7e7ad257723143440c2
cruz-welandi <cruzwelandi@email.com>
2023-10-28 14:59:24 +0100

    correction beug

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function Navbar() {
     return(
         <div className="bg-cyan-200 py-4 flex justify-center items-center">
-            <ul className="flex gap-x-4">
+            <ul className="flex gap-x-4 text-xl">
                 <Link to={'/Home'}>Home</Link>
                 <Link to={'/Students'}>Students</Link>
                 <Link to={'/Login'}>Login</Link>
