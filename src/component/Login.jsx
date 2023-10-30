8bc5f8c57c671ff8d0600b3467b51c842ca378cb
cruz-welandi <cruzwelandi@email.com>
2023-10-30 03:05:37 +0100

    recuperer et envoie des données à une api

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,60 +1,99 @@
+import React,{useState} from "react";
+import Axios from 'axios';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 function Login() {
+    const [data, setData] = useState ({
+        nom: '',
+        prenom: '',
+        email: '',
+        phone: '',
+        libeleClasse: '',
+        genre: '',
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setData({ ...data, [name]: value});
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            // Envoie les données du formulaire à l'API Strapi
+            const response = await Axios.post('http://localhost:1337/api/eleves', {data});
+            console.log('Réponse de l\'API Strapi :', response.data);
+        } catch (error) {
+            if (error.response) {
+              // La requête a été effectuée, mais le serveur a renvoyé une erreur
+              console.error('Réponse du serveur avec erreur:', error.response.data);
+            } else if (error.request) {
+              // La requête n'a pas pu être effectuée
+              console.error('La requête n\'a pas pu être effectuée:', error.request);
+            } else {
+              // Une autre erreur s'est produite
+              console.error('Erreur lors de la requête:', error.message);
+            }
+          }
+
+    };
+
     return (
         <div>
             <Navbar/>
             <div>
                 <div>
-                    <form className="p-5 flex flex-col justify-center gap-y-3 items-center">
+                    <h1 className="text-center p-2 text-xl font-bold">Enregister un élève</h1>
+                    <form className="p-5 flex flex-col justify-center gap-y-3 items-center" onSubmit={handleSubmit}>
                         <div className="flex  flex-col gap-y-2 w-[200px]">
                             <label>
                                Nom(s)
                             </label>
-                            <input type="text" name="Nom" className="bg-gray-300 rounded h-10"/>
+                            <input type="text" name="nom" value={data.nom} onChange={handleChange} className="bg-gray-300 rounded h-10"/>
                         </div>
                         <div className="flex  flex-col gap-y-2 w-[200px]">
                             <label>
                                 Prenom(s)
                             </label>
-                            <input type="text" name="Premon" className="bg-gray-300 rounded h-10"/>
+                            <input type="text" name="prenom" value={data.prenom} onChange={handleChange} className="bg-gray-300 rounded h-10"/>
                         </div>
                         <div className="flex  flex-col gap-y-2 w-[200px]">
                             <label>
                                 Email
                             </label>
-                            <input type="email" name="Email" className="bg-gray-300 rounded h-10"/>
+                            <input type="email" name="email" value={data.email} onChange={handleChange} className="bg-gray-300 rounded h-10"/>
                         </div>
                         <div className="flex  flex-col gap-y-2 w-[200px]">
                             <label>
                                 Tel
                             </label>
-                            <input type="text" className="bg-gray-300 rounded h-10"/>
+                            <input type="text" name="phone" value={data.phone} onChange={handleChange} className="bg-gray-300 rounded h-10"/>
                         </div>
                         <div className="flex  flex-col gap-y-2 w-[200px]">
                             <label>
                                 Classe
                             </label>
-                            <input type="text" name="Classe" className="bg-gray-300 rounded h-10"/>
+                            <input type="text" name="libeleClasse" value={data.libeleClasse} onChange={handleChange} className="bg-gray-300 rounded h-10"/>
                         </div>
                         <div className="flex gap-x-5 ">
                             <div className="flex gap-x-3">
-                                <input type="radio" name="Genre" className="bg-gray-300 rounded"/>
+                                <input type="radio" name="genre" value="mascumin" className="bg-gray-300 rounded" onChange={handleChange}/>
                                 <label>
                                     Masculin
                                 </label>
                                
                             </div>
                             <div className="flex gap-x-3"> 
-                                <input type="radio" name="Genre" className="bg-gray-300 rounded"/>
+                                <input type="radio" name="genre" value="feminin" className="bg-gray-300 rounded" onChange={handleChange}/>
                                 <label>
                                     Feminin
                                 </label>
                                
                             </div>
                         </div>
-                        <button className="p-2 bg-cyan-300 w-[100px] rounded text-lg" type="submit" name="">
+                        <button className="p-2 bg-cyan-300 w-[100px] rounded text-lg" type="submit">
                                Enregister
                         </button>
                     </form>
--- src/component/Students.jsx
+++ src/component/Students.jsx
@@ -1,11 +1,57 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import React, { useEffect, useState } from "react";
+import Axios from 'axios';
 
 function Students() {
+        
+    const [error, setError] = useState(null);
+    const [dataEleve, setDataEleve] = useState([]);
+
+    useEffect (() => {
+        Axios
+            .get("http://localhost:1337/api/eleves")
+            .then((response) => setDataEleve(response.data.data))
+            .catch((error) => setError(error));
+    }, []);
+
+    if (error) {
+        return <div>An error occured: {error.message}</div>;
+    }
+   
+
     return (
         <div>
             <Navbar/>
-           
+            <div className="flex justify-center p-4">
+                <table >
+                <caption className="font-bold text-2xl mb-4">
+                        Liste des élèves enregisté
+                </caption>
+                <thead className="uppercase bg-gray-50">
+                    <tr className="px-3 py-2 flex  gap-x-5">
+                        <th  scope="col" className="">Nom(s)</th>
+                        <th  scope="col" className="">Prenom(s)</th>
+                        <th  scope="col" className="">Adresse mail</th>
+                        <th  scope="col" className="">Classe</th>
+                        <th  scope="col" className="">Numéro de telephone</th>
+                        <th  scope="col" className="">Genre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dataEleve.map(({id,attributes}) => (
+                        <tr key={id} className=" border-b px-3 py-3  flex justify-between">
+                            <td scope="row" className="">{attributes.nom}</td>
+                            <td scope="row" className="">{attributes.prenom}</td>
+                            <td scope="row" className="">{attributes.email}</td>
+                            <td scope="row" className="">{attributes.libeleClasse}</td>
+                            <td scope="row" className="">{attributes.phone}</td>
+                            <td scope="row" className="">{attributes.genre}</td>
+                        </tr>
+                    ))}
+                </tbody>
+                </table>
+            </div>
             <Footer/>
         </div>
     )
